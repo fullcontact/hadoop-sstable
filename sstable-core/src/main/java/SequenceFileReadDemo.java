30039c8857e86793f95ea70e9c296f5d77fa9047
Vineet Gadodia <vg8632@yp.com>
2014-11-25 10:28:05 -0800

    Fix for using hadoop 2.0.0

--- sstable-core/src/main/java/SequenceFileReadDemo.java
+++ sstable-core/src/main/java/SequenceFileReadDemo.java
@@ -18,7 +18,6 @@ public class SequenceFileReadDemo {
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(URI.create(uri), conf);
     Path path = new Path(uri);
-
     SequenceFile.Reader reader = null;
     try {
       reader = new SequenceFile.Reader(fs, path, conf);
