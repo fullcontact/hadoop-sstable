d9ec42853e7ca93e2d91994208317ca76095ebae
Eric Entzel <eric@ubermac.net>
2015-02-24 09:52:58 -0700

    remove unused

--- sstable-core/src/main/java/com/fullcontact/cassandra/io/util/FileUtils.java
+++ sstable-core/src/main/java/com/fullcontact/cassandra/io/util/FileUtils.java
@@ -21,36 +21,13 @@ import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.MappedByteBuffer;
-import java.nio.file.Files;
-import java.nio.file.AtomicMoveNotSupportedException;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.text.DecimalFormat;
-import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.cassandra.config.Config;
-import org.apache.cassandra.config.DatabaseDescriptor;
-import org.apache.cassandra.db.BlacklistedDirectories;
-import org.apache.cassandra.db.Keyspace;
-import org.apache.cassandra.io.FSError;
-import org.apache.cassandra.io.FSReadError;
-import org.apache.cassandra.io.FSWriteError;
-import org.apache.cassandra.io.sstable.CorruptSSTableException;
-import org.apache.cassandra.service.StorageService;
-
 public class FileUtils
 {
     private static final Logger logger = LoggerFactory.getLogger(FileUtils.class);
-    private static final double KB = 1024d;
-    private static final double MB = 1024*1024d;
-    private static final double GB = 1024*1024*1024d;
-    private static final double TB = 1024*1024*1024*1024d;
-
-    private static final DecimalFormat df = new DecimalFormat("#.##");
-
     private static final Method cleanerMethod;
 
     static
@@ -69,141 +46,6 @@ public class FileUtils
         cleanerMethod = m;
     }
 
-    public static void createHardLink(File from, File to)
-    {
-        if (to.exists())
-            throw new RuntimeException("Tried to create duplicate hard link to " + to);
-        if (!from.exists())
-            throw new RuntimeException("Tried to hard link to file that does not exist " + from);
-
-        try
-        {
-            Files.createLink(to.toPath(), from.toPath());
-        }
-        catch (IOException e)
-        {
-            throw new FSWriteError(e, to);
-        }
-    }
-
-    public static File createTempFile(String prefix, String suffix, File directory)
-    {
-        try
-        {
-            return File.createTempFile(prefix, suffix, directory);
-        }
-        catch (IOException e)
-        {
-            throw new FSWriteError(e, directory);
-        }
-    }
-
-    public static File createTempFile(String prefix, String suffix)
-    {
-        return createTempFile(prefix, suffix, new File(System.getProperty("java.io.tmpdir")));
-    }
-
-    public static void deleteWithConfirm(String file)
-    {
-        deleteWithConfirm(new File(file));
-    }
-
-    public static void deleteWithConfirm(File file)
-    {
-        assert file.exists() : "attempted to delete non-existing file " + file.getName();
-        if (logger.isDebugEnabled())
-            logger.debug("Deleting " + file.getName());
-        try
-        {
-            Files.delete(file.toPath());
-        }
-        catch (IOException e)
-        {
-            throw new FSWriteError(e, file);
-        }
-    }
-
-    public static void renameWithOutConfirm(String from, String to)
-    {
-        try
-        {
-            atomicMoveWithFallback(new File(from).toPath(), new File(to).toPath());
-        }
-        catch (IOException e)
-        {
-            if (logger.isTraceEnabled())
-                logger.trace("Could not move file "+from+" to "+to, e);
-        }
-    }
-
-    public static void renameWithConfirm(String from, String to)
-    {
-        renameWithConfirm(new File(from), new File(to));
-    }
-
-    public static void renameWithConfirm(File from, File to)
-    {
-        assert from.exists();
-        if (logger.isDebugEnabled())
-            logger.debug((String.format("Renaming %s to %s", from.getPath(), to.getPath())));
-        // this is not FSWE because usually when we see it it's because we didn't close the file before renaming it,
-        // and Windows is picky about that.
-        try
-        {
-            atomicMoveWithFallback(from.toPath(), to.toPath());
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(String.format("Failed to rename %s to %s", from.getPath(), to.getPath()), e);
-        }
-    }
-
-    /**
-     * Move a file atomically, if it fails, it falls back to a non-atomic operation
-     * @param from
-     * @param to
-     * @throws IOException
-     */
-    private static void atomicMoveWithFallback(Path from, Path to) throws IOException
-    {
-        try
-        {
-            Files.move(from, to, StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.ATOMIC_MOVE);
-        }
-        catch (AtomicMoveNotSupportedException e)
-        {
-            logger.debug("Could not do an atomic move", e);
-            Files.move(from, to, StandardCopyOption.REPLACE_EXISTING);
-        }
-
-    }
-    public static void truncate(String path, long size)
-    {
-        RandomAccessFile file;
-
-        try
-        {
-            file = new RandomAccessFile(path, "rw");
-        }
-        catch (FileNotFoundException e)
-        {
-            throw new RuntimeException(e);
-        }
-
-        try
-        {
-            file.getChannel().truncate(size);
-        }
-        catch (IOException e)
-        {
-            throw new FSWriteError(e, path);
-        }
-        finally
-        {
-            closeQuietly(file);
-        }
-    }
-
     public static void closeQuietly(Closeable c)
     {
         try
@@ -214,55 +56,6 @@ public class FileUtils
         catch (Exception e)
         {
             logger.warn("Failed closing " + c, e);
-        }
-    }
-
-    public static void close(Closeable... cs) throws IOException
-    {
-        close(Arrays.asList(cs));
-    }
-
-    public static void close(Iterable<? extends Closeable> cs) throws IOException
-    {
-        IOException e = null;
-        for (Closeable c : cs)
-        {
-            try
-            {
-                if (c != null)
-                    c.close();
-            }
-            catch (IOException ex)
-            {
-                e = ex;
-                logger.warn("Failed closing stream " + c, ex);
-            }
-        }
-        if (e != null)
-            throw e;
-    }
-
-    public static String getCanonicalPath(String filename)
-    {
-        try
-        {
-            return new File(filename).getCanonicalPath();
-        }
-        catch (IOException e)
-        {
-            throw new FSReadError(e, filename);
-        }
-    }
-
-    public static String getCanonicalPath(File file)
-    {
-        try
-        {
-            return file.getCanonicalPath();
-        }
-        catch (IOException e)
-        {
-            throw new FSReadError(e, file);
         }
     }
 
@@ -292,98 +85,6 @@ public class FileUtils
         }
     }
 
-    public static void createDirectory(String directory)
-    {
-        createDirectory(new File(directory));
-    }
-
-    public static void createDirectory(File directory)
-    {
-        if (!directory.exists())
-        {
-            if (!directory.mkdirs())
-                throw new FSWriteError(new IOException("Failed to mkdirs " + directory), directory);
-        }
-    }
-
-    public static boolean delete(String file)
-    {
-        File f = new File(file);
-        return f.delete();
-    }
-
-    public static void delete(File... files)
-    {
-        for ( File file : files )
-        {
-            file.delete();
-        }
-    }
-
-    public static void deleteAsync(final String file)
-    {
-        Runnable runnable = new Runnable()
-        {
-            public void run()
-            {
-                deleteWithConfirm(new File(file));
-            }
-        };
-        StorageService.tasks.execute(runnable);
-    }
-
-    public static String stringifyFileSize(double value)
-    {
-        double d;
-        if ( value >= TB )
-        {
-            d = value / TB;
-            String val = df.format(d);
-            return val + " TB";
-        }
-        else if ( value >= GB )
-        {
-            d = value / GB;
-            String val = df.format(d);
-            return val + " GB";
-        }
-        else if ( value >= MB )
-        {
-            d = value / MB;
-            String val = df.format(d);
-            return val + " MB";
-        }
-        else if ( value >= KB )
-        {
-            d = value / KB;
-            String val = df.format(d);
-            return val + " KB";
-        }
-        else
-        {
-            String val = df.format(value);
-            return val + " bytes";
-        }
-    }
-
-    /**
-     * Deletes all files and subdirectories under "dir".
-     * @param dir Directory to be deleted
-     * @throws FSWriteError if any part of the tree cannot be deleted
-     */
-    public static void deleteRecursive(File dir)
-    {
-        if (dir.isDirectory())
-        {
-            String[] children = dir.list();
-            for (String child : children)
-                deleteRecursive(new File(dir, child));
-        }
-
-        // The directory is now empty so now it can be smoked
-        deleteWithConfirm(dir);
-    }
-
     public static void skipBytesFully(DataInput in, int bytes) throws IOException
     {
         int n = 0;
@@ -395,50 +96,4 @@ public class FileUtils
             n += skipped;
         }
     }
-
-    public static void skipBytesFully(DataInput in, long bytes) throws IOException
-    {
-        long n = 0;
-        while (n < bytes)
-        {
-            int m = (int) Math.min(Integer.MAX_VALUE, bytes - n);
-            int skipped = in.skipBytes(m);
-            if (skipped == 0)
-                throw new EOFException("EOF after " + n + " bytes out of " + bytes);
-            n += skipped;
-        }
-    }
-
-    public static void handleCorruptSSTable(CorruptSSTableException e)
-    {
-        if (DatabaseDescriptor.getDiskFailurePolicy() == Config.DiskFailurePolicy.stop_paranoid)
-            StorageService.instance.stopTransports();
-    }
-    
-    public static void handleFSError(FSError e)
-    {
-        switch (DatabaseDescriptor.getDiskFailurePolicy())
-        {
-            case stop_paranoid:
-            case stop:
-                StorageService.instance.stopTransports();
-                break;
-            case best_effort:
-                // for both read and write errors mark the path as unwritable.
-                BlacklistedDirectories.maybeMarkUnwritable(e.path);
-                if (e instanceof FSReadError)
-                {
-                    File directory = BlacklistedDirectories.maybeMarkUnreadable(e.path);
-                    if (directory != null)
-                        Keyspace.removeUnreadableSSTables(directory);
-                }
-                break;
-            case ignore:
-                // already logged, so left nothing to do
-                break;
-            default:
-                throw new IllegalStateException();
-        }
-    }
-
 }
